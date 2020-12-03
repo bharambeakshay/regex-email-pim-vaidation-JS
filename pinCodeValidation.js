318128c82c005adf57aef5602ca39fd40ff5aee0
Akshay <bharambeakshay1997@gmail.com>
2020-12-04 00:54:29 +0530

    Added: regex validation for 6 digit pincode

--- pinCodeValidation.js
+++ pinCodeValidation.js
@@ -1,1 +1,17 @@
-console.log("Pin Code Validations")
+//uc1 pincode validation for  6 digit number
+let pincodeRegex = RegExp("[1-9]{1}[0-9]{5}");
+function PincodeValidation(pincode) {
+    if (pincodeRegex.test(pincode) && pincode.length === 6) {
+        console.log(pincode + "  is in valid format");
+    }
+    else throw "Invalid Pincode Format";
+}
+try {
+    //readline-sync waits for users response and make it synchronised
+    var readline = require('readline-sync');
+    let pincode = readline.question(" Please Enter the pincode for validation: ");
+    PincodeValidation(pincode);
+}
+catch (e) {
+    console.log(e);
+}
