8a69bfb777a589c58caecad5fc0a203a07f1a953
Akshay <bharambeakshay1997@gmail.com>
2020-12-11 13:06:56 +0530

    [Akshay]  Refactor . UC3 for email validating third  part

--- emailValidation.js
+++ emailValidation.js
@@ -1,8 +1,9 @@
 const readline = require('readline-sync');
 //email format- abc.xyz@bridgelabz.co.in .xyz & .in optional & rest all mandatory
 //uc1 validating first part of the email i.e. abc
-//uc2 changing email pattern for validating second part i.e. @bridgelabs, akshay@bridgelabz
-let emailRegex = RegExp("^[A-Za-z]{3,}[@][A-Za-z0-9]{1,}$")
+//uc2 changing email pattern for validating second part i.e. @bridgelabs
+//uc3 validating third part .co in email pattern
+let emailRegex = RegExp("^[A-Za-z]{3,}[@][A-Za-z0-9]{1,}[.][a-zA-Z0-9]{2,3}$")
 function EmailValidation(email) {
     if (emailRegex.test(email)) {
         console.log(email + "  is in valid format");
@@ -11,7 +12,7 @@ function EmailValidation(email) {
     else throw "Incorect Email Format";
 }
 try {
-    let email = readline.question(" Please Enter the email for validation: ");
+    let email = readline.question(" Please Enter the email for validation");
     EmailValidation(email);
 }
 catch (e) {
