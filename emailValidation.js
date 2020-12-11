fde256e10186095c17cc4aa0143de47c229dbe61
Akshay <bharambeakshay1997@gmail.com>
2020-12-11 13:02:00 +0530

    [Akshay]  Add . UC2 for email validating second part

--- emailValidation.js
+++ emailValidation.js
@@ -1,9 +1,8 @@
 const readline = require('readline-sync');
-
-//uc1 validating first part of the email 
-
-let emailRegex = RegExp("^[A-Za-z]{3,}$")
-
+//email format- abc.xyz@bridgelabz.co.in .xyz & .in optional & rest all mandatory
+//uc1 validating first part of the email i.e. abc
+//uc2 changing email pattern for validating second part i.e. @bridgelabs, akshay@bridgelabz
+let emailRegex = RegExp("^[A-Za-z]{3,}[@][A-Za-z0-9]{1,}$")
 function EmailValidation(email) {
     if (emailRegex.test(email)) {
         console.log(email + "  is in valid format");
@@ -11,9 +10,7 @@ function EmailValidation(email) {
     }
     else throw "Incorect Email Format";
 }
-
 try {
-
     let email = readline.question(" Please Enter the email for validation: ");
     EmailValidation(email);
 }
